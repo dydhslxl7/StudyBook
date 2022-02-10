8d0639d679476a1140722eef6f845ee41ae8ac1f
dydhslxl7 <dydhslxl7@naver.com>
2022-02-10 22:09:37 +0900

    practice 02-10

--- Do_it_Algorithm_java/Chapter_1/table_1_1.java
+++ Do_it_Algorithm_java/Chapter_1/table_1_1.java
@@ -18,6 +18,8 @@ public class table_1_1 {
         if(c > max) max = c;
 
         System.out.println("최댓값은 " + max + "입니다.");
+
+        stdIn.close();
     }
 
     static int max3_2(int a, int b, int c) {
@@ -44,5 +46,42 @@ public class table_1_1 {
         System.out.println("max3_2(1,3,2) = " + max3_2(1,3,2));
         System.out.println("max3_2(2,3,3) = " + max3_2(2,3,3));
         System.out.println("max3_2(1,2,3) = " + max3_2(1,2,3));
+    }
+
+    // Q1
+    static int max4(int a, int b, int c, int d) {
+        int max = a;
+        if(b > max)
+            max = b;
+        if(c > max)
+            max = c;
+        if(d > max)
+            max = d;
+        
+        return max;
+    }
+
+    // Q2
+    static int min3(int a, int b, int c) {
+        int min = a;
+        if(b < min)
+            min = b;
+        if(c < min)
+            min = c;
+        
+        return min;
+    }
+
+    // Q3
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if(b < min)
+            min = b;
+        if(c < min)
+            min = c;
+        if(d < min)
+            min = d;
+        
+        return min;
     }
 }
