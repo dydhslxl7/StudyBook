f007a96002910e446699f9802cb1df278a9d298d
dydhslxl7 <dydhslxl7@naver.com>
2022-02-15 22:41:30 +0900

    22-02-15

--- Do_it_Algorithm_java/Chapter_1/Multi99Table.java
+++ Do_it_Algorithm_java/Chapter_1/Multi99Table.java
@@ -1,5 +1,7 @@
 package Do_it_Algorithm_java.Chapter_1;
 
+import java.util.Scanner;
+
 public class Multi99Table {
     public static void main(String[] args) {
         // System.out.println("----- 곱셈표 -----");
@@ -11,7 +13,9 @@ public class Multi99Table {
         //     System.out.println();
         // }
         
-        q12();
+        // q12();
+        // q13();
+        q14();
     }
 
     //연습문제
@@ -37,5 +41,45 @@ public class Multi99Table {
                 System.out.print("\n---+---------------------------");
             System.out.println();
         }
+    }
+
+    static void q13() {
+        for (int i = 0; i <= 9; i++) {            
+            for (int j = 0; j <= 9; j++) {
+                if(i == 0)
+                {
+                    if(j == 0)
+                        System.out.print("  |");
+                    else
+                        System.out.printf("%3d", j);
+                }
+                else
+                {
+                    if(j == 0)
+                        System.out.print(" "+i+"|");
+                    else
+                        System.out.printf("%3d", i + j);
+                }
+            }
+            if(i == 0)
+                System.out.print("\n---+---------------------------");
+            System.out.println();
+        }
+    }
+
+    static void q14() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("단 수 : ");
+        int n = in.nextInt();
+        
+        for (int i = 0; i < n; i++) {            
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        in.close();
     }
 }
---
+++ Do_it_Algorithm_java/Chapter_1/TriangleLB.java
@@ -0,0 +1,95 @@
+package Do_it_Algorithm_java.Chapter_1;
+
+import java.util.Scanner;
+
+public class TriangleLB {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+
+        System.out.println("왼쪽 아래가 직각인 이등변 삼각형을 출력합니다.");
+
+        do {
+            System.out.print("몇 단 삼각형입니까? : ");
+            n = in.nextInt();
+        } while (n <= 0);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        in.close();
+
+        triangleLU(5);
+        triangleRU(5);
+        triangleRB(5);
+    }
+
+    // 연습문제
+    // 왼쪽 위가 직각인 이등변 삼각형을 출력
+    static void triangleLU(int n) {
+        System.out.println("왼쪽 위가 직각인 이등변 삼각형을 출력합니다.");
+
+        do {
+            System.out.println("몇 단 삼각형입니까? : "+n);
+        } while (n <= 0);
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+    // 오른쪽 위가 직각인 이등변 삼각형을 출력
+    static void triangleRU(int n) {
+        System.out.println("오른쪽 위가 직각인 이등변 삼각형을 출력합니다.");
+
+        do {
+            System.out.println("몇 단 삼각형입니까? : "+n);
+        } while (n <= 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(i < j)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    // 오른쪽 아래가 직각인 이등변 삼각형을 출력
+    static void triangleRB(int n) {
+        System.out.println("오른쪽 아래가 직각인 이등변 삼각형을 출력합니다.");
+
+        do {
+            System.out.println("몇 단 삼각형입니까? : "+n);
+        } while (n <= 0);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= 1; j--) {
+                if(i >= j)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    //ing
+    static void spira(int n) {
+        int last_n = (n-1) * 2 + 1;
+
+        int n1;
+        for (int i = 1; i <= n; i++) {
+            n1 = (i-1) * 2 + 1;
+        }
+    }
+}
