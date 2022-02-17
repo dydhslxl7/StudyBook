2ae88be7b72d96f5bd9998116ccac1d122e5f9a9
dydhslxl7 <dydhslxl7@naver.com>
2022-02-17 22:56:52 +0900

    22-02-17

---
+++ Do_it_Algorithm_java/chap02/IntArrayInit.java
@@ -0,0 +1,11 @@
+package Do_it_Algorithm_java.chap02;
+
+public class IntArrayInit {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "] = " + a[i]);
+        }
+    }
+}
