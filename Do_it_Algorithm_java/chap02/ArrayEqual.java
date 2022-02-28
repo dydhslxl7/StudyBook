fbcf98ba1319779820a7789b99e2c0c120ca2a33
dydhslxl7 <dydhslxl7@naver.com>
2022-02-28 14:32:17 +0000

    22-02-28 2

--- Do_it_Algorithm_java/chap02/ArrayEqual.java
+++ Do_it_Algorithm_java/chap02/ArrayEqual.java
@@ -1,5 +1,8 @@
 package Do_it_Algorithm_java.chap02;
 
+import java.util.Scanner;
+// 두 배열이 같은가를 판단
+
 public class ArrayEqual {
     public static void main(String[] args) {
         // 두 배열 a, b의 모든 요소가 같은가?
