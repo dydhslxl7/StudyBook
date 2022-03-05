a91fb19faf7602abf407d3b2a4454975d7606176
dydhslxl7 <dydhslxl7@naver.com>
2022-03-05 15:16:15 +0900

    22-03-05 CardConvRev-cardConv

--- Do_it_Algorithm_java/chap02/CardConvRev.java
+++ Do_it_Algorithm_java/chap02/CardConvRev.java
@@ -52,4 +52,23 @@ public class CardConvRev {
 
         in.close();
     }
+
+    static int cardConv(int x, int r, char[] d) {
+        int digits = 0;
+        String dchar = "0123456789ABCDEFGHHIJKLMOPQRSTUVWXYZ";
+
+        do {
+            d[digits++] = dchar.charAt(x % r); 
+            x /= r;
+        } while (x != 0);
+
+        char temp;
+        for (int i = 0; i < (digits / 2); i++) {
+            temp = d[i];
+            d[i] = d[digits-i];
+            d[digits-i] = temp;
+        }
+
+        return digits;
+    }
 }
