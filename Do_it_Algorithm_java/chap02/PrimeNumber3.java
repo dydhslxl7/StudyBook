3827726ae130e4524743084fb77b000031375fff
dydhslxl7 <dydhslxl7@naver.com>
2022-03-14 21:28:27 +0900

    22-03-14

--- Do_it_Algorithm_java/chap02/PrimeNumber3.java
+++ Do_it_Algorithm_java/chap02/PrimeNumber3.java
@@ -10,10 +10,12 @@ public class PrimeNumber3 {
         prime[ptr++] = 2;
         prime[ptr++] = 3;
 
-        for (int n = 5; n <= 1000; n += 2) {
+        for (int n = 5; n <= 1000; n += 2)
+        {
             boolean flag = false;
 
-            for (int i = 1; prime[i] * prime[i] <= n; i++) {
+            for (int i = 1; prime[i] * prime[i] <= n; i++)
+            {
                 counter += 2;
 
                 if(n % prime[i] == 0)
@@ -22,6 +24,19 @@ public class PrimeNumber3 {
                     break;
                 }
             }
+
+            if(!flag)
+            {
+                prime[ptr++] = n;
+                counter++;
+            }
         }
+
+        for (int i = 0; i < ptr; i++)
+        {
+            System.out.println(prime[i]);
+        }
+
+        System.out.println("곱셈과 나눗셈을 수행한 횟수 : " + counter);
     }    
 }
