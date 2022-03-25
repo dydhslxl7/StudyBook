f3f90a2cbc1094b690e582c1dcefe5c456f85d00
dydhslxl7 <dydhslxl7@naver.com>
2022-03-25 20:27:37 +0900

    PhysicalExamination_2 ing

---
+++ Do_it_Algorithm_java/chap02/PhysicalExamination_2.java
@@ -0,0 +1,47 @@
+package Do_it_Algorithm_java.chap02;
+
+public class PhysicalExamination_2 {
+
+    static class YMD
+    {
+        private int y;  // 년
+        private int m;  // 월(1 ~ 12)
+        private int d;  // 일(1 ~ 31)
+
+        YMD(int y, int m, int d)
+        {    
+            this.y = y;
+            this.m = m;
+            this.d = d;
+        }
+
+        int get_y() {
+            return y;
+        }
+
+        int get_m() {
+            return m;
+        }
+
+        int get_d() {
+            return d;
+        }
+
+        static int after(int n)
+        {
+            return n;    
+        }
+
+        static int before(int n)
+        {
+
+            return n;    
+        }
+    }
+
+    
+
+    public static void main(String[] args) {
+        // 나중에 다시
+    }
+}
