0e6e058f823979331582a66004e7f501f3e55213
dydhslxl7 <dydhslxl7@naver.com>
2022-04-04 21:19:11 +0900

    SeqSearchSen_127_line update

--- Do_it_Algorithm_java/chap03/SeqSearchSen.java
+++ Do_it_Algorithm_java/chap03/SeqSearchSen.java
@@ -124,7 +124,7 @@ public class SeqSearchSen {
         for(i = 0; i < n; i++) {
             System.out.printf("   |");
             for (int j = 0; j < i+1; j++) {
-                if(j != i)
+                if(j != 0 && (j != (n-1)) && j != i)
                     System.out.printf("   ");
                 else if(j == 0)
                     System.out.printf(" <-");
