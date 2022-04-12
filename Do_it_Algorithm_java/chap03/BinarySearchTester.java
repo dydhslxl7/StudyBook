7de1c7e6538303b461291c39f121b89382c59d21
dydhslxl7 <dydhslxl7@naver.com>
2022-04-12 22:54:23 +0900

    BinarySearchTester_complete

--- Do_it_Algorithm_java/chap03/BinarySearchTester.java
+++ Do_it_Algorithm_java/chap03/BinarySearchTester.java
@@ -16,5 +16,24 @@ public class BinarySearchTester {
 
         System.out.print("x[0] : ");    // 배열의 첫 요소를 먼저 입력합니다.
         x[0] = stdIn.nextInt();
+
+        for (int i = 1; i < num; i++) {
+            do {
+                System.out.print("x[" + i + "] : ");
+                x[i] = stdIn.nextInt();
+            } while (x[i] < x[i-1]);    // 바로 앞의 요소보다 작으면 다시 입력합니다.
+        }
+
+        System.out.print("검색할 값 : "); // 키 값을 입력 받음
+        int ky = stdIn.nextInt();
+
+        int idx = Arrays.binarySearch(x, ky);   // 배열 x에서 키 값이 ky인 요소를 검색
+
+        if (idx < 0)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+
+        stdIn.close();
     }
 }
